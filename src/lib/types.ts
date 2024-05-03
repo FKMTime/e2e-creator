5b88c5e5a6631211ce4bc71c8eba5665986df79a
filipton <filipton12@gmail.com>
2024-05-03 23:03:39 +0200

    feat: init e2e test creator (sveltekit) feat: simple tests.json viewer

---
+++ src/lib/types.ts
@@ -0,0 +1,81 @@
+export interface TestsRoot {
+    dumpStateAfterTest: boolean;
+    cards: Record<string, Card>;
+    buttons: Record<string, number[]>;
+    tests: Test[];
+}
+
+export interface Card {
+    canCompete: boolean;
+    name: string;
+    registrantId: number;
+    wcaId: string;
+}
+
+export interface Test {
+    name: string;
+    sleepBetween: number;
+    steps: Step[];
+}
+
+export interface SleepStep {
+    type: "Sleep";
+    data: number;
+}
+
+export interface ScanCardStep {
+    type: "ScanCard";
+    data: number;
+}
+
+export interface SnapshotStep {
+    type: "Snapshot";
+    data: null;
+}
+
+export interface ResetStateStep {
+    type: "ResetState";
+    data: null;
+}
+
+export interface SolveTimeStep {
+    type: "SolveTime";
+    data: number;
+}
+
+export interface SolveTimeRngStep {
+    type: "SolveTimeRng";
+    data: null;
+}
+
+export interface ButtonStep {
+    type: "Button";
+    data: { name: string; time: number };
+}
+
+export interface DelegateResolveStep {
+    type: "DelegateResolve";
+    data: { shouldScanCards: boolean; penalty: number; value: number };
+}
+
+export interface VerifySolveTimeStep {
+    type: "VerifySolveTime";
+    data: { time?: number; penalty: number };
+}
+
+export interface VerifyDelegateSentStep {
+    type: "VerifyDelegateSent";
+    data: null;
+}
+
+export type Step =
+    | SleepStep
+    | ScanCardStep
+    | SnapshotStep
+    | ResetStateStep
+    | SolveTimeStep
+    | SolveTimeRngStep
+    | ButtonStep
+    | DelegateResolveStep
+    | VerifySolveTimeStep
+    | VerifyDelegateSentStep;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import type { TestsRoot, Step } from "$lib/types";
+
+    let files: FileList;
+    let testRoot: TestsRoot | null = null;
+
+    $: if (files) {
+        handleFiles();
+    }
+
+    async function handleFiles() {
+        let file = files[0];
+        let text = await file.text();
+        let json: TestsRoot = JSON.parse(text);
+        testRoot = json;
+    }
+</script>
+
+{#if !files}
+    <input bind:files type="file" accept=".json" />
+{/if}
+
+{#if testRoot}
+    {#each Object.entries(testRoot.cards) as [id, card]}
+        {id}: {card.name} | {card.wcaId} | {card.registrantId} | {card.canCompete}
+        <br />
+    {/each}
+
+    <br />
+
+    {#each Object.entries(testRoot.buttons) as [name, pins]}
+        {name}: {pins.join(", ")}
+        <br />
+    {/each}
+
+    <br />
+
+    {#each testRoot.tests as test}
+        {test.name} <br />
+        <ul>
+            {#each test.steps as step}
+                <li>{step.type}</li>
+                {#if step.data}
+                    {JSON.stringify(step.data)}
+                {/if}
+            {/each}
+        </ul>
+
+        <br />
+    {/each}
+{/if}
